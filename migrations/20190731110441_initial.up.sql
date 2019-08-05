397343a5769c4a8589911f650f7e9e63708a6fca
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-08-05 09:48:21 +0200

    Return only recent events (-3 hours, +60 days).

--- migrations/20190731110441_initial.up.sql
+++ migrations/20190731110441_initial.up.sql
@@ -1,3 +1,7 @@
+--- protected schema
+CREATE SCHEMA protected;
+COMMENT ON SCHEMA protected IS 'The schema for protected queries';
+
 --- eventzimmer schema
 CREATE SCHEMA eventzimmer;
 COMMENT ON SCHEMA eventzimmer IS 'The schema for the eventzimmer event aggregation service';
@@ -40,12 +44,17 @@ COMMENT ON COLUMN eventzimmer.events.name IS 'The name that the event will be di
 COMMENT ON COLUMN eventzimmer.events.url IS 'The url that the event should be linked to. This can be an anchor or a fully qualified resource identifier.';
 
 -- Use ST_Point to create a geometry for every latitude / longitude in the locations table.
-CREATE VIEW eventzimmer.locations_geom AS SELECT
+CREATE VIEW protected.locations_geom AS SELECT
   name,
   (SELECT CAST(ST_SetSRID(ST_Point(longitude, latitude), 4326) AS GEOGRAPHY)) AS geom
 FROM eventzimmer.locations;
 
-COMMENT ON VIEW eventzimmer.locations_geom IS 'The list of geometries for locations';
+COMMENT ON VIEW protected.locations_geom IS 'The list of geometries for locations';
+
+-- Events no older than 3 hours up to 60 days
+CREATE VIEW protected.recent_events AS SELECT * FROM eventzimmer.events WHERE starts_at >= current_timestamp - interval '3 hours' AND starts_at <= current_timestamp + interval '60 days';
+
+COMMENT ON VIEW protected.recent_events IS 'Recent events';
 
 -- Specify that radius must be positive and no larger than 25000 meters.
 CREATE DOMAIN radius AS integer CHECK(VALUE > 0 AND VALUE <= 25000);
@@ -53,25 +62,30 @@ CREATE DOMAIN radius AS integer CHECK(VALUE > 0 AND VALUE <= 25000);
 -- Function for filtering events by latitude / longitude
 CREATE FUNCTION eventzimmer.events_by_radius(latitude float, longitude float, radius radius default 15000) RETURNS SETOF eventzimmer.events
     AS $$
-    SELECT * FROM eventzimmer.events WHERE location IN 
-    (SELECT name FROM eventzimmer.locations_geom WHERE ST_DWithin(
+    SELECT * FROM protected.recent_events WHERE location IN 
+    (SELECT name FROM protected.locations_geom WHERE ST_DWithin(
       geom,
       (SELECT CAST(ST_SetSRID(ST_Point(longitude, latitude), 4326) AS GEOGRAPHY)),
       radius
     ))
 $$ LANGUAGE SQL STABLE;
 
+COMMENT ON FUNCTION eventzimmer.events_by_radius IS 'Filters event by radius';
+
 -- Permission systems
 CREATE ROLE web_anon nologin;
 CREATE ROLE aggregator nologin;
 
 GRANT USAGE ON SCHEMA eventzimmer TO web_anon, aggregator;
+GRANT USAGE ON SCHEMA protected TO web_anon;
 
 GRANT SELECT ON eventzimmer.events TO web_anon;
 GRANT SELECT ON eventzimmer.sources TO web_anon;
 GRANT SELECT ON eventzimmer.locations TO web_anon;
-GRANT SELECT ON eventzimmer.locations_geom TO web_anon;
 GRANT EXECUTE ON FUNCTION eventzimmer.events_by_radius TO web_anon;
+
+GRANT SELECT ON protected.locations_geom TO web_anon;
+GRANT SELECT ON protected.recent_events TO web_anon;
 
 GRANT INSERT ON eventzimmer.events TO aggregator;
 GRANT SELECT ON eventzimmer.events TO aggregator;
