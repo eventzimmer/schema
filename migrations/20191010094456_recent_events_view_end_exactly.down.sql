40a9f00552049da17a76599c9a64e4cf5f5ffd50
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-10-10 12:45:00 +0200

    Use CREATE OR REPLACE instead of TRANSACTION because it makes live simpler.

--- migrations/20191010094456_recent_events_view_end_exactly.down.sql
+++ migrations/20191010094456_recent_events_view_end_exactly.down.sql
@@ -1,5 +1,2 @@
 --- undo transaction
-BEGIN;
-DROP VIEW protected.recent_events;
-CREATE VIEW protected.recent_events AS SELECT * FROM eventzimmer.events WHERE starts_at >= current_timestamp - interval '3 hours' AND starts_at <= current_timestamp + interval '60 days';
-COMMIT;
+CREATE OR REPLACE VIEW protected.recent_events AS SELECT * FROM eventzimmer.events AS events WHERE events.starts_at >= current_timestamp - interval '3 hours' AND events.starts_at <= current_timestamp + interval '60 days';
--- migrations/20191010094456_recent_events_view_end_exactly.up.sql
+++ migrations/20191010094456_recent_events_view_end_exactly.up.sql
@@ -1,5 +1,2 @@
 --- replace old with new view
-BEGIN;
-DROP VIEW protected.recent_events;
-CREATE VIEW protected.recent_events AS SELECT * FROM eventzimmer.events WHERE starts_at >= current_timestamp - interval '3 hours' AND starts_at < (date_trunc('month', current_timestamp::date) + interval '2 month')::date;
-COMMIT;
+CREATE OR REPLACE VIEW protected.recent_events AS SELECT * FROM eventzimmer.events WHERE starts_at >= current_timestamp - interval '3 hours' AND starts_at < (date_trunc('month', current_timestamp::date) + interval '2 month')::date;
